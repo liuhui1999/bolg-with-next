40e908a07453d7bb93725081a18a1974ef6b10ad
刘辉 <1003785694@qq.com>
2023-07-26 16:47:33 +0800

    no message

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,4 @@
-import { format, parseISO } from 'date-fns';
 import { GetStaticProps } from 'next';
-import Link from 'next/link';
 import React from 'react';
 import Layout from '../components/Layout';
 import { getAllPosts } from '../lib/api';
@@ -65,27 +63,6 @@ export const Index = ({ posts }: IndexProps): JSX.Element => {
           </div >
         </div>
 
-
-        {/* {posts.map((post) => (
-        <article key={post.slug} className="mt-12">
-          <p className="mb-1 text-sm text-gray-500 dark:text-gray-400">
-            {format(parseISO(post.date as string), 'MMMM dd, yyyy')}
-          </p>
-          <h1 className="mb-2 text-xl">
-            <Link as={`/posts/${post.slug}`} href={`/posts/[slug]`}>
-              <a className="text-gray-900 dark:text-white dark:hover:text-blue-400">
-                {post.title}
-              </a>
-            </Link>
-          </h1>
-          <p className="mb-3">{post.description}</p>
-          <p>
-            <Link as={`/posts/${post.slug}`} href={`/posts/[slug]`}>
-              <a>Read More</a>
-            </Link>
-          </p>
-        </article>
-      ))} */}
       </Layout>
       <People></People>
     </>
